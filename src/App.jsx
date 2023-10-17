e1ef0ce7a5b1a314a5c675cb22e580bb7321c9bb
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-10-17 05:49:46 +0200

    Styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import styled from "styled-components"
+
+//Return new component
+const H1 = styled.h1`
+  font-size: 40px;
+  font-weight: 600;
+  background-color: lightcoral;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+`
+
 function App() {
   return (
     <div>
-      Hello New Project
+      <H1>The Wild Oasis</H1>
+      <Button>Click</Button>
     </div>
   )
 }
