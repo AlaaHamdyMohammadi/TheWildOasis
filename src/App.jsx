a9f69da2cf7cd23a6816b3e8740ee04d90e7008d
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-10-18 02:54:45 +0200

    Implement Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import styled from "styled-components";
-import GlobalStyles from "./styles/GlobalStyles";
-
-//Return new component
-const H1 = styled.h1`
-  font-size: 40px;
-  font-weight: 600;
-  background-color: var(--color-grey-900);
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: var(--color-grey-300);
-  color: white;
-`;
-
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import GlobalStyles from './styles/GlobalStyles'
+import Dashboard from '../src/pages/Dashboard';
+import Bookings from '../src/pages/Bookings';
+import Cabins from '../src/pages/Cabins';
+import Users from '../src/pages/Users';
+import Settings from '../src/pages/Settings';
+import Account from '../src/pages/Account';
+import Login from '../src/pages/Login';
+import PageNotFound from '../src/pages/PageNotFound';
 function App() {
   return (
     <>
-      <GlobalStyles />
-      <div>
-        <H1>The Wild Oasis</H1>
-        <Button>Click</Button>
-      </div>
+      <GlobalStyles/>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard></Dashboard>} />
+            <Route path="bookings" element={<Bookings></Bookings>} />
+            <Route path="cabins" element={<Cabins></Cabins>} />
+            <Route path="users" element={<Users></Users>} />
+            <Route path="settings" element={<Settings></Settings>} />
+            <Route path="account" element={<Account></Account>} />
+            <Route path="login" element={<Login></Login>} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
     </>
   );
 }
 
-export default App;
+export default App
