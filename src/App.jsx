e8d59c162f4f7865c46d6ba1d0592bb19b6f6837
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-10-18 03:30:20 +0200

    Implement AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
-import GlobalStyles from './styles/GlobalStyles'
-import Dashboard from '../src/pages/Dashboard';
-import Bookings from '../src/pages/Bookings';
-import Cabins from '../src/pages/Cabins';
-import Users from '../src/pages/Users';
-import Settings from '../src/pages/Settings';
-import Account from '../src/pages/Account';
-import Login from '../src/pages/Login';
-import PageNotFound from '../src/pages/PageNotFound';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import GlobalStyles from "./styles/GlobalStyles";
+import Dashboard from "../src/pages/Dashboard";
+import Bookings from "../src/pages/Bookings";
+import Cabins from "../src/pages/Cabins";
+import Users from "../src/pages/Users";
+import Settings from "../src/pages/Settings";
+import Account from "../src/pages/Account";
+import Login from "../src/pages/Login";
+import PageNotFound from "../src/pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 function App() {
   return (
     <>
-      <GlobalStyles/>
-        <BrowserRouter>
-          <Routes>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard></Dashboard>} />
             <Route path="bookings" element={<Bookings></Bookings>} />
@@ -21,12 +23,13 @@ function App() {
             <Route path="users" element={<Users></Users>} />
             <Route path="settings" element={<Settings></Settings>} />
             <Route path="account" element={<Account></Account>} />
+            </Route>
             <Route path="login" element={<Login></Login>} />
             <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
 
-export default App
+export default App;
