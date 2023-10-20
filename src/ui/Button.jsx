6808878f17c0538137c4600e60fd6f3a969c072c
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-10-20 03:35:46 +0200

    Install React-Hook-Form

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import styled, { css } from "styled-components";
 
 const sizes = {
@@ -48,3 +47,19 @@ const variations = {
     }
   `,
 };
+
+const Button = styled.button`
+  border: none;
+  border-radius: var(--border-radius-sm);
+  box-shadow: var(--shadow-sm);
+
+  ${(props) => sizes[props.size]}
+  ${(props) => variations[props.variation]}
+`;
+
+Button.defaultProps = {
+  variation: "primary",
+  size: "medium",
+};
+
+export default Button;
