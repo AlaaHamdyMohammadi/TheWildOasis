18434aaf06f4324aed42d0aa9842619062ebddc0
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-10-30 00:46:50 +0200

    Update

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,14 +1,16 @@
 /* eslint-disable no-unused-vars */
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login } from "../../services/apiAuth";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
  
 export function useLogin(){
     const navigate = useNavigate();
+    const queryClient = useQueryClient();
     const {isLoading: isLogging, mutate: loginMethod} = useMutation({
         mutationFn: ({email, password}) => login({email, password}),
-        onSuccess: () => {
+        onSuccess: (user) => {
+            queryClient.setQueryData(['user'], user.user)
             navigate("/dashboard", { replace: true });
             //toast.success(`${data} successfully Login`);
 
