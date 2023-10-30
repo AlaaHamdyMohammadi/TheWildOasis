2cb42edcfee92754ffb1519d96943054508d4967
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-10-30 19:25:53 +0200

    Building App Header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,20 +1,25 @@
-import styled from "styled-components"
-import Logout from "../features/authentication/Logout";
+import styled from "styled-components";
+import HeaderMenu from "./HeaderMenu";
+import UserAvatar from "../features/authentication/UserAvatar";
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
-
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid black;
+
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
 function Header() {
-    return (
-        <StyledHeader>
-            HEADER
-            <Logout />
-        </StyledHeader>
-    )
+  return (
+    <StyledHeader>
+      <UserAvatar />
+      <HeaderMenu />
+    </StyledHeader>
+  );
 }
 
-export default Header
+export default Header;
